5e132c7644e8d305f7df391a18d6aade35994d3c
asciisummer <romelsoviguidi@gmail.com>
2021-10-09 01:36:45 +0000

    init repo and make makefile

---
+++ include/audiocorp.h
@@ -0,0 +1,7 @@
+#ifndef __audiocorp__
+#define __audiocorp__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "../include/audiocorp.h"
+
+int main()
+{
+    printf("ce n'est que le debut");
+    return (0);
+}
